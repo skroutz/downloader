f7420c75b07381729434c3e04411a7791976b5c1
Agis Anastasopoulos <agis@skroutz.gr>
2017-10-19 17:54:37 +0300

    Refactor job.NewAggregation

--- job/aggregation.go
+++ job/aggregation.go
@@ -14,18 +14,11 @@ type Aggregation struct {
 }
 
 func NewAggregation(id string, limit int) (*Aggregation, error) {
-	a := new(Aggregation)
-
 	if id == "" {
-		return a, errors.New("Aggregation ID cannot be empty")
+		return nil, errors.New("Aggregation ID cannot be empty")
 	}
-
 	if limit <= 0 {
-		return a, errors.New("Aggregation limit must be greater than 0")
+		return nil, errors.New("Aggregation limit must be greater than 0")
 	}
-
-	a.ID = id
-	a.Limit = limit
-
-	return a, nil
+	return &Aggregation{ID: id, Limit: limit}, nil
 }
