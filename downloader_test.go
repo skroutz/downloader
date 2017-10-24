d692317e910acc3c31a91247bcfbd9a9fe79061e
Agis Anastasopoulos <agis@skroutz.gr>
2017-10-24 12:55:10 +0300

    notifier: No download URL for failed jobs
    Fixes T6685

--- downloader_test.go
+++ downloader_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -22,6 +23,8 @@ import (
 	"golang.skroutz.gr/skroutz/downloader/notifier"
 )
 
+type testJob map[string]interface{}
+
 var (
 	mu           sync.Mutex
 	originalArgs = make([]string, len(os.Args))
@@ -34,17 +37,19 @@ var (
 
 	// An HTTP file server that serves the contents of testdata/.
 	// Used to test Processor.
-	fsHost     = "localhost"
-	fsPort     = "9718"
-	fsPath     = "/testdata/"
+	fsHost = "localhost"
+	fsPort = "9718"
+	fsPath = "/testdata/"
+	// used to instrument fileServer
 	fsChan     = make(chan int, 10)
 	fileServer = newFileServer(fmt.Sprintf("%s:%s", fsHost, fsPort), fsChan)
 
-	csHost   = "localhost"
-	csPort   = "9894"
-	csPath   = "/callback/"
-	csChan   = make(chan []byte)
-	cbServer = newCallbackServer(fmt.Sprintf("%s:%s", csHost, csPort), csChan)
+	csHost = "localhost"
+	csPort = "9894"
+	csPath = "/callback/"
+	// callbacks from cbServer are emitted to this channel
+	callbacks = make(chan []byte, 1000)
+	cbServer  = newCallbackServer(fmt.Sprintf("%s:%s", csHost, csPort), callbacks)
 
 	timeout = 5 * time.Second
 )
@@ -108,7 +113,7 @@ func TestMain(m *testing.M) {
 	sigCh <- os.Interrupt // shutdown Notifier
 
 	wg.Wait()
-	//purgeRedis()
+	flushRedis()
 	os.Exit(result)
 }
 
@@ -118,15 +123,16 @@ func TestResourceExists(t *testing.T) {
 	var err error
 
 	// Test job creation (APIServer)
-	downloadURL := fmt.Sprintf("http://%s:%s%ssample-1.jpg", fsHost, fsPort, fsPath)
-	callbackURL := fmt.Sprintf("http://%s:%s%s", csHost, csPort, csPath)
-	jobData := map[string]interface{}{
+	jobData := testJob{
 		"aggr_id":      "asemas",
 		"aggr_limit":   1,
-		"url":          downloadURL,
-		"callback_url": callbackURL}
+		"url":          downloadURL("sample-1.jpg"),
+		"callback_url": callbackURL()}
 
-	postJob(jobData, t)
+	err = postJob(jobData)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	// Test callback mechanism (Notifier)
 	var parsedCB notifier.CallbackInfo
@@ -134,7 +140,7 @@ func TestResourceExists(t *testing.T) {
 	select {
 	case <-time.After(timeout):
 		t.Fatal("Callback request receive timeout")
-	case cb := <-csChan:
+	case cb := <-callbacks:
 		err = json.Unmarshal(cb, &parsedCB)
 		if err != nil {
 			t.Fatalf("Error parsing callback response: %s | %s", err, string(cb))
@@ -215,18 +221,21 @@ FILECHECK:
 func TestResourceDontExist(t *testing.T) {
 	var parsedCB notifier.CallbackInfo
 
-	job := map[string]interface{}{
+	job := testJob{
 		"aggr_id":      "foobar",
 		"aggr_limit":   1,
-		"url":          fmt.Sprintf("http://%s:%s%si-dont-exist.foo", fsHost, fsPort, fsPath),
-		"callback_url": fmt.Sprintf("http://%s:%s%s", csHost, csPort, csPath)}
+		"url":          downloadURL("i-dont-exist.foo"),
+		"callback_url": callbackURL()}
 
-	postJob(job, t)
+	err := postJob(job)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	select {
 	case <-time.After(timeout):
 		t.Fatal("Callback request receive timeout")
-	case cb := <-csChan:
+	case cb := <-callbacks:
 		err := json.Unmarshal(cb, &parsedCB)
 		if err != nil {
 			t.Fatalf("Error parsing callback response: %s | %s", err, string(cb))
@@ -237,12 +246,11 @@ func TestResourceDontExist(t *testing.T) {
 		if !strings.HasSuffix(parsedCB.Error, "404") {
 			t.Fatalf("Expected Error to end with '404': %s", parsedCB.Error)
 		}
+		if parsedCB.DownloadURL != "" {
+			t.Fatalf("Expected DownloadURL to be empty: %#v", parsedCB)
+		}
 		if parsedCB.Extra != "" {
 			t.Fatalf("Expected Extra to be empty: %#v", parsedCB)
-		}
-		if !strings.HasPrefix(parsedCB.DownloadURL, "http://localhost/") {
-			t.Fatalf("Expected DownloadURL to begin with 'http://localhost/': %#v",
-				parsedCB)
 		}
 	}
 }
@@ -251,7 +259,7 @@ func TestResourceDontExist(t *testing.T) {
 func TestTransientDownstreamError(t *testing.T) {
 	var parsedCB notifier.CallbackInfo
 
-	job := map[string]interface{}{
+	job := testJob{
 		"aggr_id":      "murphy",
 		"aggr_limit":   1,
 		"url":          fmt.Sprintf("http://%s:%s%ssample-1.jpg", fsHost, fsPort, fsPath),
@@ -261,13 +269,16 @@ func TestTransientDownstreamError(t *testing.T) {
 	fsChan <- http.StatusServiceUnavailable  // 2nd try
 	// 3rd try will succeed
 
-	postJob(job, t)
+	err := postJob(job)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	select {
 	// bump timeout cause we also have to wait for the backoffs
 	case <-time.After(timeout + (5 * time.Second)):
 		t.Fatal("Callback request receive timeout")
-	case cb := <-csChan:
+	case cb := <-callbacks:
 		err := json.Unmarshal(cb, &parsedCB)
 		if err != nil {
 			t.Fatalf("Error parsing callback response: %s | %s", err, string(cb))
@@ -276,7 +287,7 @@ func TestTransientDownstreamError(t *testing.T) {
 			t.Fatal("Expected Success to be true")
 		}
 		if parsedCB.Error != "" {
-			t.Fatalf("Expected Error to be empty", parsedCB.Error)
+			t.Fatalf("Expected Error to be empty, was '%s'", parsedCB.Error)
 		}
 		if parsedCB.Extra != "" {
 			t.Fatalf("Expected Extra to be empty: %#v", parsedCB)
@@ -304,7 +315,7 @@ func start(args ...string) {
 // blocks until the API server is up
 func waitForServer(port string) {
 	time.Sleep(500 * time.Millisecond)
-	conn, err := net.DialTimeout("tcp", "localhost:"+port, 3*time.Second)
+	conn, err := net.DialTimeout("tcp", "localhost:"+port, timeout)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -312,22 +323,23 @@ func waitForServer(port string) {
 }
 
 // Creates a Job by executing a request to the API server
-func postJob(data map[string]interface{}, t *testing.T) {
+func postJob(data map[string]interface{}) error {
 	// TODO: get download endpoint from config
 	uri := fmt.Sprintf("http://%s:%s/download", apiHost, apiPort)
 
 	v, _ := json.Marshal(data)
 	resp, err := apiClient.Post(uri, "application/json", bytes.NewBuffer(v))
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 201 {
 		info, _ := httputil.DumpResponse(resp, true)
 		log.Println(string(info))
-		t.Fatalf("Expected 201 response, got %d", resp.StatusCode)
+		return errors.New(fmt.Sprintf("Expected 201 response, got %d", resp.StatusCode))
 	}
+	return nil
 }
 
 func newFileServer(addr string, ch chan int) *http.Server {
@@ -381,4 +393,12 @@ func flushRedis() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func downloadURL(filename string) string {
+	return fmt.Sprintf("http://%s:%s%s%s", fsHost, fsPort, fsPath, filename)
+}
+
+func callbackURL() string {
+	return fmt.Sprintf("http://%s:%s%s", csHost, csPort, csPath)
 }
