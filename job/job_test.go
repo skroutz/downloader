60a0c8de2960a37b570018d1977a8cbf4ad9f99c
Agis Anastasopoulos <agis.anast@gmail.com>
2017-10-26 15:09:43 +0300

    job: Implement json.Unmarshaler
    This lets us validate the given JSON, like we do with Aggregation.
    
    Fixes T6755
    Fixes T6760

---
+++ job/job_test.go
@@ -0,0 +1,41 @@
+package job
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestUnmarshalJSON(t *testing.T) {
+	tc := map[string]bool{
+		``:              true,
+		`{"foo"}`:       true,
+		`{"foo":"bar"}`: true,
+
+		// invalid url
+		`{"aggr_id":"foo", "url":"foo","callback_url":"http://foo.bar","extra":"whatever"}`: true,
+		`{"aggr_id":"foo", "url":"","callback_url":"http://foo.bar","extra":"whatever"}`:    true,
+
+		// invalid cb url
+		`{"aggr_id":"foo", "url":"http://foobar.com","callback_url":"fijfij","extra":"whatever"}`: true,
+
+		// invalid aggr_id
+		`{"aggr_id":true, "url":"http://foobar.com","callback_url":"http://foo.bar","extra":"whatever"}`: true,
+		`{"aggr_id":"", "url":"http://foobar.com","callback_url":"http://foo.bar","extra":"whatever"}`:   true,
+
+		`{"aggr_id":"foo","url":"http://foobar.com","callback_url":"http://foo.bar"}`:                     false,
+		`{"aggr_id":"foo", "url":"http://foobar.com","callback_url":"http://foo.bar","extra":"whatever"}`: false,
+		`{"aggr_id":"foo","url":"http://foobar.com","callback_url":"http://foo.bar","extra":""}`:          false,
+	}
+
+	for data, expectErr := range tc {
+		j := new(Job)
+		err := j.UnmarshalJSON([]byte(data))
+		receivedErr := (err != nil)
+		if receivedErr != expectErr {
+			if err != nil {
+				fmt.Println(err)
+			}
+			t.Errorf("Expected receivedErr to be %v for '%s'", expectErr, data)
+		}
+	}
+}
