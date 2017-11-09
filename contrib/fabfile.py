95d16d8f5f7e8577b70b12e5fe9645b03a69cf76
Christos Trochalakis <yatiohi@ideopolis.gr>
2017-11-09 13:23:37 +0200

    fabric: Add stats commands to gather node statistics

--- contrib/fabfile.py
+++ contrib/fabfile.py
@@ -7,8 +7,11 @@ $ ln -s contrib/fabfile.py
 $ fab -H dl1,dl2 deploy
 
 """
-from fabric.operations import put, sudo, local
+from json import loads as json
+
+from fabric.operations import put, sudo, local, run
 from fabric.decorators import runs_once
+from fabric.context_managers import hide
 
 @runs_once
 def build():
@@ -21,6 +24,15 @@ def restart():
     sudo('systemctl restart downloader@api.service')
     sudo('systemctl restart downloader@processor.service')
     sudo('systemctl restart downloader@notifier.service')
+
+def stats():
+    keys = ['processor', 'notifier']
+
+    with hide('output'):
+        stats = [json(run('redis-cli get stats:{}'.format(s))) for s in keys]
+        for ss in stats:
+            for s, v in json(ss).items():
+                print "{:>30} {:>10}".format(s, v)
 
 def deploy():
     build()
