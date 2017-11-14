b5f0b69ef9ca5ecfc3e15f4574c2ff75464dd2b0
Christos Trochalakis <yatiohi@ideopolis.gr>
2017-11-14 17:10:29 +0200

    test: Allow processor to read args() before swapping them
    Closes T7621

--- downloader_test.go
+++ downloader_test.go
@@ -90,6 +90,8 @@ func TestMain(m *testing.M) {
 	componentsWg.Add(1)
 	go start("processor", "--config", "config.test.json")
 
+	// Wait for processor to start before changing os.args
+	time.Sleep(10 * time.Millisecond)
 	componentsWg.Add(1)
 	go start("notifier", "--config", "config.test.json")
 
