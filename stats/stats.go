5b89bad5ce5c231db38a55a69896c8768147ef9a
Agis Anastasopoulos <a@xz0.org>
2019-04-08 11:34:02 +0300

    stats: Remove unnecessary logging
    Also improve documentation.

--- stats/stats.go
+++ stats/stats.go
@@ -4,21 +4,17 @@ package stats
 import (
 	"context"
 	"expvar"
-	"log"
 	"time"
 )
 
 // Stats is the statistics reporting entity of the downloader.
-// It acts as a deamon and reports stats using
+// It wraps an expvar.Map and acts as a deamon that reports stats using
 // the provided reportfunc on the specified interval.
 type Stats struct {
 	*expvar.Map
 	interval   time.Duration
 	reportfunc func(m *expvar.Map)
 }
-
-// Reporter encapsulates an expvar Map and acts as a metric reporting interface for each module
-//var Reporter *Stats
 
 // Run calls the report function of Stats using the specified interval/
 // It shuts down when the provided context is cancelled
@@ -28,7 +24,6 @@ func (s *Stats) Run(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			log.Println("Stats Deamon Exiting")
 			return
 		case <-tick.C:
 			s.reportfunc(s.Map)
@@ -36,12 +31,12 @@ func (s *Stats) Run(ctx context.Context) {
 	}
 }
 
-// New initializes the Reporter and start Run
+// New accepts an custom identifier for the thing being monitored, an interval
+// on which to report the metrics and the report function.
 func New(id string, interval time.Duration, report func(*expvar.Map)) *Stats {
 	var statsMap *expvar.Map
 	if val := expvar.Get(id); val != nil {
 		if newmap, ok := val.(*expvar.Map); ok {
-			log.Printf("Stats for %s reinitialized!", id)
 			statsMap = newmap
 			statsMap.Init()
 		}
